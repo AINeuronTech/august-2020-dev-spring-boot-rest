a5b4320f47314c969dbec7974f5fec902b210ffc
mrahman <rahmanww@gmail.com>
2020-09-09 12:19:30 -0400

    mysql

--- src/main/java/com/aineurontech/StudentInfoServices/rest/StudentProfile.java
+++ src/main/java/com/aineurontech/StudentInfoServices/rest/StudentProfile.java
@@ -5,11 +5,13 @@ import com.aineurontech.StudentInfoServices.entity.Student;
 import com.aineurontech.StudentInfoServices.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class StudentProfile {
 
     public StudentService studentService;
